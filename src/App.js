39621445435dfd7da2962efcdb0bbf6a6155e008
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-10-19 21:44:28 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { FriendForm } from "./FriendForm";
 import { FriendCard } from "./FriendCard";
-import { RandomIdGenerator } from "./RandomIdGenerator";
 
 function App() {
   return (
     <>
       <div className="wrapper">
         <h1>Friend List App</h1>
-        {/* <RandomIdGenerator /> */}
         <FriendForm />
         <p className="no-friends">You did not add any friends yet...</p>
         <div className="cards-wrapper">
-          <FriendCard id={RandomIdGenerator} />
+          <FriendCard />
         </div>
       </div>
     </>
--- src/FriendCard.jsx
+++ src/FriendCard.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { RandomIdGenerator } from "./RandomIdGenerator";
 
 const friends = [
   {
-    id: RandomIdGenerator(),
+    id: 123,
     firstName: "John",
     lastName: "Smith",
     age: 32,
     city: "Kaunas",
   },
   {
-    id: RandomIdGenerator(),
+    id: 132,
     firstName: "Maria",
     lastName: "Hudghes",
     age: 28,
     city: "Siauliai",
   },
   {
-    id: RandomIdGenerator(),
+    id: 112,
     firstName: "Thomas",
     lastName: "Muiller",
     age: 33,
@@ -43,18 +42,3 @@ export function FriendCard(props) {
     </>
   );
 }
-
-// export function FriendCard(props) {
-//   return (
-//     <div className="card">
-//       <div>
-//         <p>ID: {props.id}</p>
-//         <p>First Name: {props.firstName}</p>
-//         <p>Last Name: {props.lastName}</p>
-//         <p>Age: {props.age}</p>
-//         <p>City: {props.city}</p>
-//       </div>
-//       <button className="delete-btn">Delete</button>
-//     </div>
-//   );
-// }
