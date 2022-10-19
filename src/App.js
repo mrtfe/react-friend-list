1fdbd85a5137c4d5d9671825183d06773578f8c6
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-10-19 20:14:52 +0300

    random id generated to friends

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { FriendForm } from "./FriendForm";
 import { FriendCard } from "./FriendCard";
+import { v4 as uuidv4 } from "uuid";
 
 const friends = [
   { firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
@@ -16,18 +17,21 @@ function App() {
         <p className="no-friends">You did not add any friends yet...</p>
         <div className="cards-wrapper">
           <FriendCard
+            id={uuidv4()}
             firstName={friends[0].firstName}
             lastName={friends[0].lastName}
             age={friends[0].age}
             city={friends[0].city}
           />
           <FriendCard
+            id={uuidv4()}
             firstName={friends[1].firstName}
             lastName={friends[1].lastName}
             age={friends[1].age}
             city={friends[1].city}
           />
           <FriendCard
+            id={uuidv4()}
             firstName={friends[2].firstName}
             lastName={friends[2].lastName}
             age={friends[2].age}
--- src/FriendCard.jsx
+++ src/FriendCard.jsx
@@ -4,6 +4,7 @@ export function FriendCard(props) {
   return (
     <div className="card">
       <div>
+        <p>ID: {props.id}</p>
         <p>First Name: {props.firstName}</p>
         <p>Last Name: {props.lastName}</p>
         <p>Age: {props.age}</p>
