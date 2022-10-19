d55434f08f6f143b6d36571620ac3e1f2279db85
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-10-19 18:43:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>Fried List App</header>
+      <div>
+        <form>
+          <div>
+            <label>First Name</label>
+            <input placeholder="Vardenis"></input>
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input placeholder="Pavardenis"></input>
+          </div>
+          <div>
+            <label>Age</label>
+            <input placeholder="0"></input>
+          </div>
+          <div>
+            <label>City From</label>
+            <input placeholder="City Name"></input>
+          </div>
+          <button>Add Friend</button>
+        </form>
+      </div>
+    </>
   );
 }
 
---
+++ src/Friends.jsx
@@ -0,0 +1,5 @@
+const friends = [
+  { firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
+  { firstName: "Maria", lastName: "Hudghes", age: 28, city: "Siauliai" },
+  { firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
+];
