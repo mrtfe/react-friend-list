b25e2708bab766a9ef327fae80bafeac8c6e7e12
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-10-19 21:30:54 +0300

    random id generator added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 import { FriendForm } from "./FriendForm";
 import { FriendCard } from "./FriendCard";
-import { v4 as uuidv4 } from "uuid";
-
-const friends = [
-  { firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
-  { firstName: "Maria", lastName: "Hudghes", age: 28, city: "Siauliai" },
-  { firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
-];
+import { RandomIdGenerator } from "./RandomIdGenerator";
 
 function App() {
   return (
     <>
       <div className="wrapper">
         <h1>Friend List App</h1>
+        {/* <RandomIdGenerator /> */}
         <FriendForm />
         <p className="no-friends">You did not add any friends yet...</p>
         <div className="cards-wrapper">
-          <FriendCard
-            id={uuidv4()}
-            firstName={friends[0].firstName}
-            lastName={friends[0].lastName}
-            age={friends[0].age}
-            city={friends[0].city}
-          />
-          <FriendCard
-            id={uuidv4()}
-            firstName={friends[1].firstName}
-            lastName={friends[1].lastName}
-            age={friends[1].age}
-            city={friends[1].city}
-          />
-          <FriendCard
-            id={uuidv4()}
-            firstName={friends[2].firstName}
-            lastName={friends[2].lastName}
-            age={friends[2].age}
-            city={friends[2].city}
-          />
+          <FriendCard id={RandomIdGenerator} />
         </div>
       </div>
     </>
--- src/FriendCard.jsx
+++ src/FriendCard.jsx
@@ -1,16 +1,60 @@
 import React from "react";
+import { RandomIdGenerator } from "./RandomIdGenerator";
+
+const friends = [
+  {
+    id: RandomIdGenerator(),
+    firstName: "John",
+    lastName: "Smith",
+    age: 32,
+    city: "Kaunas",
+  },
+  {
+    id: RandomIdGenerator(),
+    firstName: "Maria",
+    lastName: "Hudghes",
+    age: 28,
+    city: "Siauliai",
+  },
+  {
+    id: RandomIdGenerator(),
+    firstName: "Thomas",
+    lastName: "Muiller",
+    age: 33,
+    city: "Vilnius",
+  },
+];
 
 export function FriendCard(props) {
   return (
-    <div className="card">
-      <div>
-        <p>ID: {props.id}</p>
-        <p>First Name: {props.firstName}</p>
-        <p>Last Name: {props.lastName}</p>
-        <p>Age: {props.age}</p>
-        <p>City: {props.city}</p>
-      </div>
-      <button className="delete-btn">Delete</button>
-    </div>
+    <>
+      {friends.map((friend) => (
+        <div className="card">
+          <div>
+            <p>ID: {friend.id}</p>
+            <p>First Name: {friend.firstName}</p>
+            <p>Last Name: {friend.lastName}</p>
+            <p>Age: {friend.age}</p>
+            <p>City: {friend.city}</p>
+          </div>
+          <button className="delete-btn">Delete</button>
+        </div>
+      ))}
+    </>
   );
 }
+
+// export function FriendCard(props) {
+//   return (
+//     <div className="card">
+//       <div>
+//         <p>ID: {props.id}</p>
+//         <p>First Name: {props.firstName}</p>
+//         <p>Last Name: {props.lastName}</p>
+//         <p>Age: {props.age}</p>
+//         <p>City: {props.city}</p>
+//       </div>
+//       <button className="delete-btn">Delete</button>
+//     </div>
+//   );
+// }
